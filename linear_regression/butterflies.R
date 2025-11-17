5049d3ac68abc542dddbd10a5f5b6dd2885010c0
Andrea Raaschou <andrea.raaschou@gmail.com>
2025-11-17 11:20:13 +0100

    Complete code for hand-in

--- linear_regression/butterflies.R
+++ linear_regression/butterflies.R
@@ -1,7 +1,12 @@
 # Analyzing a factorial experiment using ANOVA
 
+library(ggplot2)
+
+# Remove all variables 
+rm(list = ls())
+
 # Set working directory
-setwd("/Users/andrearaaschou/courses/BIOS15/github/")
+setwd("/Users/andrearaaschou/courses/BIOS15/github/linear_regression/")
 
 # Upload data 
 dat = read.csv("datasets/butterflies.csv")
@@ -12,21 +17,31 @@ dat$LarvalHost = paste0(dat$LarvalHost, "L")
 
 # Compute the means of the developmental time for each combination of larval and maternal host plant
 means = tapply(dat$DevelopmentTime, list(dat$MaternalHost, dat$LarvalHost), mean)
+means
 
-# Make plot to illustrate the model/data
+# Make plot to illustrate the data
+ggplot(dat, aes(x = LarvalHost,
+                y = DevelopmentTime,
+                color = MaternalHost,
+                group = MaternalHost)) +
+  stat_summary(fun = mean, geom = "line", size = 1.2) + 
+  stat_summary(fun = mean, geom = "point", size = 3) +
+  labs(x = "Larval Host Plant",
+       y = "Development Time",
+       color = "Maternal Host") +
+  theme_bw()+
+  theme(axis.text.x = element_text(size = 10, face = "italic"),
+        axis.text.y = element_text(size = 10),
+        legend.text  = element_text(size = 10, face = "italic") ) +
+  scale_color_discrete(labels = c("BarbareaM" = "Barbarea",
+                                  "BerteroaM" = "Berteroa"))+
+  scale_x_discrete(labels = c("BarbareaL" = "Barbarea",
+                              "BerteroaL" = "Berteroa"))
 
-# Could make either a boxplot with four different "boxes" or mimic the line-plot in fig 2 in the lecture notes
-# Like maternal host - color and larval host - x-axes
+# Make a histogram of the dependent variable to see if the data follows a normal distribution
+hist(dat$AdultWeight) 
 
-
-
-
-
-
-
-
-
-
-
-
-
+# Linear regression and two-way ANOVA
+m <- lm(dat$AdultWeight ~ dat$LarvalHost * dat$MaternalHost)
+anova(m)
+summary(m) 
